e7ef215bacad9b1d0496f58f9d743a076dea6eda
Marcus Vinícius Campos <campos.v.marcus@gmail.com>
2019-03-01 15:19:24 -0300

    Parsing html

--- packages/sdk-web/src/components/Message/assets/text/index.jsx
+++ packages/sdk-web/src/components/Message/assets/text/index.jsx
@@ -1,11 +1,12 @@
 import { h, Component } from 'preact';
 import { StyledContainer } from './styled'
+import parse from 'html-react-parser';
 
 export default class Text extends Component {
   render({ message }) {
     return (
       <StyledContainer message={message}>
-        {message.text}
+        {parse(message.text)}
       </StyledContainer>
     )
   }
